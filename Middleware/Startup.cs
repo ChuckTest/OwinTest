0d4cfe9d9cbac663de063120ae1d7050daf6f190
Chuck Lu <chuck.lu@qq.com>
2019-02-03 14:44:08 +0800

    add startup class and reference Microsoft.Owin.Host.SystemWeb

---
+++ Middleware/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+
+namespace MiddlewareTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+
+        {
+
+        }
+    }
+}
