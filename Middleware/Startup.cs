e8f362e1a5ea15d14f76a2f3b839ff80a0b85e29
Chuck Lu <chuck.lu@qq.com>
2019-02-03 16:07:51 +0800

    add code according http://www.codedigest.com/posts/8/understanding-and-creating-owin-middlewares---part-1

--- Middleware/Startup.cs
+++ Middleware/Startup.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.IO;
+using System.Threading.Tasks;
 using Owin;
 
 namespace MiddlewareTest
 {
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
     public class Startup
     {
         public void Configuration(IAppBuilder app)
 
         {
+            app.Use(new Func<AppFunc, AppFunc>(next => (async context =>
 
+            {
+
+                using (var writer = new StreamWriter(context["owin.ResponseBody"] as Stream))
+
+                {
+
+                    await writer.WriteAsync("<h1>MW1: Hello from inline Method middleware!</h1>");
+
+                }
+
+                await next.Invoke(context);
+
+            })));
         }
     }
 }
